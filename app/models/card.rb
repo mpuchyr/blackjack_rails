b58cc2fe59139ca9774e80957fb865b4615dfff6
mpuchyr <matt.puchyr@gmail.com>
2022-10-18 15:09:51 +0000

    Create card and deck in models

---
+++ app/models/card.rb
@@ -0,0 +1,15 @@
+class Card
+  def initialize(name, value, suit)
+    @name = name
+    @value = value
+    @suit = suit
+  end
+
+  def card_name
+    return "#{@name} of #{suit}"
+  end
+
+  def get_value
+    return @value
+  end
+end
---
+++ app/models/deck.rb
@@ -0,0 +1,32 @@
+class Deck
+  def initialize
+    @cards = []
+    suits = ["diamonds", "hearts", "spades", "clubs"]
+    face_cards = {
+      "Ace" => 1,
+      "King" => 10,
+      "Queen" => 10,
+      "Jack" => 10
+    }
+
+    suits.each do |suit|
+      2.upto(10) do |i|
+        card = Card.new(name = i, value = i, suit = suit)
+        @cards.push(card)
+      end
+
+      face_cards.each do |key, value|
+        card = Card.new(name = key, value = value, suit = suit)
+        @cards.push(card)
+      end
+    end
+  end
+
+  def shuffle
+    @cards = @cards.shuffle
+  end
+
+  def deal_card
+    return @cards.pop
+  end
+end
